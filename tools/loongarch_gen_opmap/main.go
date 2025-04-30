b9bc88c0fb14f7c74c783a31ddcf479003405e68
Jia Qingtong <jiaqingtong@huawei.com>
2025-04-30 12:30:12 +0800

    loongarch: fix vldrepl.w

--- tools/loongarch_gen_opmap/main.go
+++ tools/loongarch_gen_opmap/main.go
@@ -221,7 +221,7 @@ func argToProcessor(mnemonic string, arg *common.Arg) string {
 		}
 		if mnemonic == "ll.w" || mnemonic == "sc.w" || mnemonic == "vstelm.w" ||
 			mnemonic == "ll.d" || mnemonic == "sc.d" || mnemonic == "xvldrepl.w" ||
-			mnemonic == "ldptr.w" || mnemonic == "stptr.w" ||
+			mnemonic == "ldptr.w" || mnemonic == "stptr.w" || mnemonic == "vldrepl.w" ||
 			mnemonic == "ldptr.d" || mnemonic == "stptr.d" {
 			return fmt.Sprintf("Sscaled(%d, %d, %d)", arg.Slots[0].Offset, arg.Slots[0].Width, 2)
 		}
