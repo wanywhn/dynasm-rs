78bc3a08e230bb2e80b7a23aad7f746498f0a4cf
Jia Qingtong <jiaqingtong@huawei.com>
2025-04-30 20:58:14 +0800

    loongarch: add testing

---
+++ testing/tests/loongarch_0.rs
@@ -0,0 +1,7 @@
+
+#![allow(unused_imports)]
+
+use dynasmrt::dynasm;
+use dynasmrt::DynasmApi;
+
+include!("gen_loongarch/loongarch_tests_0.rs.gen");
---
+++ testing/tests/loongarch_6.rs
@@ -0,0 +1,7 @@
+
+#![allow(unused_imports)]
+
+use dynasmrt::dynasm;
+use dynasmrt::DynasmApi;
+
+include!("gen_loongarch/loongarch_tests_6.rs.gen");
