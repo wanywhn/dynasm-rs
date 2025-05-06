d9588b026a0ce4d845250dd56c26dbd92b188633
Jia Qingtong <jiaqingtong@huawei.com>
2025-05-06 22:58:35 +0800

    loongarch: add example

---
+++ doc/examples/hello-world/src/loongarch64.rs
@@ -0,0 +1,40 @@
+use dynasmrt::{dynasm, DynasmApi, DynasmLabelApi};
+
+use std::{io, slice, mem};
+use std::io::Write;
+
+fn main() {
+    let mut ops = dynasmrt::loongarch::Assembler::new().unwrap();
+    let string = "Hello World!";
+
+    dynasm!(ops
+        ; .arch loongarch64
+        ; ->hello:
+        ; .bytes string.as_bytes()
+        ; .align 8
+        ; ->print:
+        ; .u64 print as _
+    );
+
+    let hello = ops.offset();
+    dynasm!(ops
+        ; .arch loongarch64
+        ; pcaddi a0, ->hello
+        ; addi.d a1, zero, string.len() as i32
+        ; ld.d t0, zero, ->print
+        ; jirl ra, t0, 0
+        ; jirl zero, ra, 0
+    );
+
+    let buf = ops.finalize().unwrap();
+
+    let hello_fn: extern "C" fn() -> bool = unsafe { mem::transmute(buf.ptr(hello)) };
+
+    assert!(hello_fn());
+}
+
+pub extern "C" fn print(buffer: *const u8, length: u64) -> bool {
+    io::stdout()
+        .write_all(unsafe { slice::from_raw_parts(buffer, length as usize) })
+        .is_ok()
+}
